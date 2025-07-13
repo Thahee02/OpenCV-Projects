6d415e4dee49fdf17baf93108fc4f7cf0dfc74cc
Thahee02 <developerthahee@gmail.com>
2025-07-13 22:04:42 +0530

    project: hand tracking project completed

---
+++ hand_tracking/hand_tracking.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+import time
+
+cam = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, img = cam.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.circle(img, (cx, cy), 10, (0, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow("Hand Tracking", img)
+    cv2.waitKey(1)
